bdceb65777f7d630716a02ab4f76e1e9b39603fb
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-08-22 16:34:20 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import Tonconnects from '../tonconnect-manifest.json'
+import TonConnects from '../tonconnect-manifest.json';
+const manifestUrl = TonConnects.url;
 
-
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+// const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TonConnectUIProvider manifestUrl={Tonconnects}>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App /> 
   </TonConnectUIProvider>
 
