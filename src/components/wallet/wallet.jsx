4507d648828b427dc153ca60a8722500bdd73fd3
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-08-22 12:13:57 +0500

    Update wallet.jsx

--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -36,7 +36,7 @@ const Home = ({ data, loading , fetchAccountData, myId }) => {
       if (response.ok) {
         const data = await response.json();
         // Assuming the response structure from CoinGecko:
-        const price = data.data;
+        const price = data.data.coins[0].price;;
         setTonPrice(price); // Ensure `setTonPrice` is defined and available in your context
       } else {
         console.error("Failed to fetch account data");
