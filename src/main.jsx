58905ad642cf7ae2fe0203de5d39fd902dbcb451
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-08-22 16:43:38 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
-import TonConnects from '../tonconnect-manifest.json'
-const manifestUrl = TonConnects.url;
+
 
 // const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+const manifestUrl = 'https://tgram-ten.vercel.app/tonconnect-manifest.json'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
