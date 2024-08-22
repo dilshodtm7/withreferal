6d395fc57a0b9297c89423d68dadf4cc02821593
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-08-22 16:36:11 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
-import Tonconnects from '../tonconnect-manifest.json'
+import TonConnects from '../tonconnect-manifest.json'
 const manifestUrl = TonConnects.url;
 
 // const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
